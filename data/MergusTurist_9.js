f56c5626091a2ff91a92fced9990105fc72f6c49
PabloAguiarSaboya <47189938+PabloAguiarSaboya@users.noreply.github.com>
2019-02-04 14:30:38 -0200

    Add files via upload

---
+++ data/MergusTurist_9.js
@@ -0,0 +1,11 @@
+var json_MergusTurist_9 = {
+"type": "FeatureCollection",
+"name": "MergusTurist_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nome": "Cataratas_dos Couros" }, "geometry": { "type": "Point", "coordinates": [ -47.750040676891039, -14.276837957751418 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nome": "Almécegas II" }, "geometry": { "type": "Point", "coordinates": [ -47.604749769352992, -14.186515522799867 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nome": "Almécegas I" }, "geometry": { "type": "Point", "coordinates": [ -47.596245295554027, -14.184983276750732 ] } },
+{ "type": "Feature", "properties": { "id": "4", "nome": "Cachoeira_do Papagaio" }, "geometry": { "type": "Point", "coordinates": [ -47.627673044338614, -14.249071544823114 ] } }
+]
+}
